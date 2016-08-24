215f8f6140c547c3416be0d74e10df53324ec5a1
Jabonija99 <jabonija99@yahoo.com>
2016-08-23 20:54:25 -0700

    Slick2d practice

---
+++ Training/Jarone/Slick Java/Slick_Tut01_Setup/src/javagame/Menu.java
@@ -0,0 +1,36 @@
+/*
+ * Project created by Jarone Jabonillo.
+ */
+package javagame;
+
+import org.newdawn.slick.*; //imports all slick libraries
+import org.newdawn.slick.state.*; //imports state/scene libraries
+
+//Inherits the basic game state class
+public class Menu extends BasicGameState{
+    //Constructor
+    public Menu(int state){
+    
+    
+    }
+    
+    //Initializes window; parameters(GameContainer, StateBasedGame)
+    public void init(GameContainer gc, StateBasedGame stGame) throws SlickException{
+    
+    }
+    
+    //Draws objects on screen
+    public void render(GameContainer gc, StateBasedGame stGame, Graphics g) throws SlickException{
+    
+    }
+    
+    //Updates images on screen
+    public void update(GameContainer gc, StateBasedGame stGame, int delta) throws SlickException{
+        
+    }
+    
+    //Returns the Id of the state
+    public int getID(){
+        return 0;
+    }
+}
---
+++ Training/Jarone/Slick Java/Slick_Tut01_Setup/src/slick_tut01_setup/Slick_Tut01_Setup.java
@@ -0,0 +1,11 @@
+
+package slick_tut01_setup;
+
+
+public class Slick_Tut01_Setup {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
